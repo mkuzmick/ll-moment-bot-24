a4287ebbbed6494404c9be9f099120e3ad8c89c5
ll-studio <ll-studio@ll-studios-Mac-Pro.local>
2023-10-05 16:20:39 -0400

    Updates:

--- src/ll-bots/transcription-bot/index.js
+++ src/ll-bots/transcription-bot/index.js
@@ -67,3 +67,56 @@ module.exports.transcribeFile = async function (options) {
     return ({ status: "complete" });
 }
 
+
+// module.exports.transcribeFolder = async function (options) {
+
+
+//     const openai = new OpenAI({
+//         apiKey: process.env.OPENAI_API_KEY, 
+//     });
+
+
+//     let start = new Date().getTime(); // Get current time in milliseconds
+    
+//     let filePath = options.filePath; // Assuming you pass the file path in the options
+
+//     // 1. Check file extension
+//     if (!/\.(mov|MOV|mp4|m4v)$/.test(filePath)) {
+//         throw new Error("Unsupported file format");
+//     }
+
+//     // 2. Convert video file to audio-only using ffmpeg
+//     let outputFilePath = path.join(
+//         path.dirname(filePath),
+//         path.basename(filePath, path.extname(filePath)) + ".m4a"
+//     );
+
+//     let result = spawnSync('ffmpeg', ['-i', filePath, '-vn', '-b:a', '196k', outputFilePath]);
+//     if (result.error) {
+//         throw result.error;
+//     }
+
+
+
+//     const transcription = await openai.audio.transcriptions.create({
+//         file: fs.createReadStream(outputFilePath),
+//         model: "whisper-1",
+//     });
+    
+//     console.log(transcription.text);
+
+//     // Save the transcription to .json and .txt
+//     let baseNameWithoutExtension = path.basename(filePath, path.extname(filePath));
+//     let transcriptionJsonPath = path.join(path.dirname(filePath), baseNameWithoutExtension + ".json");
+//     let transcriptionTxtPath = path.join(path.dirname(filePath), baseNameWithoutExtension + ".txt");
+
+//     fs.writeFileSync(transcriptionJsonPath, JSON.stringify(transcription, null, 4));
+//     fs.writeFileSync(transcriptionTxtPath, transcription.text);
+
+//     let stop = new Date().getTime();
+//     let durationInMilliseconds = stop - start; 
+//     console.log(`Request took ${durationInMilliseconds} milliseconds`);
+
+//     return ({ status: "complete" });
+// }
+
