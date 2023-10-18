561dbd8f2ca1aac89605a21a389a5a140a796a81
ll-studio <ll-studio@ll-studios-Mac-Pro.local>
2023-10-17 20:27:35 -0400

    Updates:

--- src/ll-bots/index-bot/message-handler.js
+++ src/ll-bots/index-bot/message-handler.js
@@ -1,9 +1,9 @@
-const { blue, yellow, cyan, magenta, gray } = require('../../ll-modules/ll-utilities/mk-utilities')
+const llog= require('../../ll-modules/ll-utilities/ll-logs')
 // add this logic back in somewhere to handle links
 const { resourceFromMessageLink } = require(`../resource-bot`);
 const makeGif = require(`../gif-bot/make-gif`);
 const momentBot = require(`../moment-bot`)
-
+const huntResponse = require('../open-ai-bot/hunt-response-1')
 exports.hello = async ({ message, say }) => {
     // say() sends a message to the channel where the event was triggered
     await say(`the bot is running, <@${message.user}>!`);
@@ -13,17 +13,25 @@ exports.rocket = async ({ message, say }) => {
     await say(`thanks for the :rocket:, <@${message.user}>`);
 }
 
-exports.parseAll = async ({ message, say }) => {
-    magenta(`parsing all messages, including this one from ${message.channel}`)
+exports.parseAll = async ({ client, message, say }) => {
+    llog.magenta(`parsing all messages, including this one from ${message.channel}`)
     if (BOT_CONFIG.channels.includes(message.channel)) {
-        blue(`handling message because ${message.channel} is one of \n${JSON.stringify(BOT_CONFIG.channels, null, 4)}`)
-        yellow(message)
+        llog.blue(`handling message because ${message.channel} is one of \n${JSON.stringify(BOT_CONFIG.channels, null, 4)}`)
+        llog.yellow(message)
         const result = await momentBot.momentMessageListener(message);
-        blue(result)
+        llog.blue(result)
+    } else if ( message.channel_type == "im" ) {
+        llog.magenta(`handling message because ${message.channel} is a DM`)
+        llog.yellow(message)
+        let result = await client.conversations.history({channel: message.channel, limit: 10})
+        llog.magenta(result)
+        let openAiResult = await huntResponse({ text: message.text, messages: result.messages });
+        llog.magenta(openAiResult)
+        let slackResult = await say(openAiResult.choices[0].message.content);
     } else {
-        magenta(`some other message we aren't handling now--uncomment message-handler line 27 to get the json`)
-        blue(`message wasn't in array ${JSON.stringify(BOT_CONFIG.channels, null, 4)}`)
-        yellow(message)
+        llog.magenta(`some other message we aren't handling now--uncomment message-handler line 27 to get the json`)
+        llog.blue(`message wasn't in array ${JSON.stringify(BOT_CONFIG.channels, null, 4)}`)
+        llog.yellow(message)
     }
 }
 
