a0353d2313a772e841913c406a4f7c4ecf260d77
mk <mk@mks-macbook-pro.lan>
2023-12-10 09:37:26 -0500

    Updates:

--- src/handlers/message-handler.js
+++ src/handlers/message-handler.js
@@ -9,7 +9,9 @@ const huntResponse = require('../bots/open-ai-bot/hunt-response-1')
 const bots = [
     {
         name: "gif-bot",
-        image: "",
+        trigger: "file",
+        channel_ids: ["C06995V5N94"],
+        image: "https://files.slack.com/files-pri/T0HTW3H0V-F063L8594N5/mkll_02138_a_bot_version_of_shakespeare_realistic_closeup_c3af60d3-3f31-4cff-a8a9-94ec517a8d76.png?pub_secret=353634cc30",
         function: async ({ client, message, say }) => { 
             llog.blue("got gif-bot request", message)
             await say("gif-bot will respond")
@@ -17,13 +19,15 @@ const bots = [
     },
     {
         name: "director-bot",
+        trigger: "name",
+        channel_ids: ["C06995V5N94"],
         image: "https://files.slack.com/files-pri/T0HTW3H0V-F063L8594N5/mkll_02138_a_bot_version_of_shakespeare_realistic_closeup_c3af60d3-3f31-4cff-a8a9-94ec517a8d76.png?pub_secret=353634cc30",
         function: async ({ client, message, say }) => {
             llog.blue("got director-bot request", message)
             const slackResult = await client.chat.postMessage({
                 channel: message.channel,
                 // text: chatResponse.choices[0].message.content,
-                text: "the Director will respond"
+                text: "the Director will respond",
                 icon_url: "https://files.slack.com/files-pri/T0HTW3H0V-F063L8594N5/mkll_02138_a_bot_version_of_shakespeare_realistic_closeup_c3af60d3-3f31-4cff-a8a9-94ec517a8d76.png?pub_secret=353634cc30",
                 username: "Director"
                 // text: "got some text, but saving secretly in the console"
@@ -45,6 +49,36 @@ exports.rocket = async ({ message, say }) => {
 
 exports.parseAll = async ({ client, message, say }) => {
     llog.magenta(`parsing all messages, including this one from ${message.channel}`)
+    for (let i = 0; i < bots.length; i++) {
+        let bot = bots[i]
+        if (bot.trigger == "file") {
+            try {
+                if (bot.channel_ids.includes(message.channel)) {
+                    await bot.function({ client, messay, say })
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        } else if (bot.trigger == "name") {
+            if (message.text.toLowerCase().includes(bot.name.toLowerCase())) {
+                // Your code here
+                if (bot.channel_ids.includes(message.channel)) {
+                    await bot.function({ client, message, say })
+                }
+            }
+            
+            
+        }
+
+        if (bots[i].channel === message.channel) {
+            llog.blue(`handling message because ${message.channel} is one of \n${JSON.stringify(BOT_CONFIG[i].channel, null, 4)}`)
+            llog.yellow(message)
+            // Call the corresponding function
+            const result = await BOT_CONFIG[i].function(message);
+            llog.blue(result)
+            // Removed return statement
+        }
+    }
 
     if (BOT_CONFIG.channels.includes(message.channel)) {
         llog.blue(`handling message because ${message.channel} is one of \n${JSON.stringify(BOT_CONFIG.channels, null, 4)}`)
@@ -62,37 +96,6 @@ exports.parseAll = async ({ client, message, say }) => {
     } else {
         llog.magenta(`some other message we aren't handling now--uncomment message-handler line 27 to get the json`)
         llog.blue(`message wasn't in array ${JSON.stringify(BOT_CONFIG.channels, null, 4)}`)
-        llog.yellow(message)
-    }
-}
-
-exports.parseAll = async ({ client, message, say }) => {
-    llog.magenta(`parsing all messages, including this one from ${message.channel}`)
-    
-    // Loop over the BOT_CONFIG array
-    for (let i = 0; i < BOT_CONFIG.length; i++) {
-        if (BOT_CONFIG[i].channel === message.channel) {
-            llog.blue(`handling message because ${message.channel} is one of \n${JSON.stringify(BOT_CONFIG[i].channel, null, 4)}`)
-            llog.yellow(message)
-            // Call the corresponding function
-            const result = await BOT_CONFIG[i].function(message);
-            llog.blue(result)
-            // Removed return statement
-        }
-    }
-
-    // Proceed with your existing logic
-    if ( message.channel_type == "im" ) {
-        llog.magenta(`handling message because ${message.channel} is a DM`)
-        llog.yellow(message)
-        let result = await client.conversations.history({channel: message.channel, limit: 10})
-        llog.magenta(result)
-        let openAiResult = await huntResponse({ text: message.text, messages: result.messages });
-        llog.magenta(openAiResult)
-        let slackResult = await say(openAiResult.choices[0].message.content);
-    } else {
-        llog.magenta(`some other message we aren't handling now--uncomment message-handler line 27 to get the json`)
-        llog.blue(`message wasn't in array ${JSON.stringify(BOT_CONFIG, null, 4)}`)
         llog.yellow(message)
     }
 }
